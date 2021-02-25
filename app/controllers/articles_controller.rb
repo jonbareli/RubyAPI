dfe5dde852d629b7830e56379d82990e065879a3
Jonathan Bar-Eli <jonbareli@gmail.com>
2021-02-25 17:53:47 -0500

    First commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+
+class ArticlesController < ApplicationController
+
+  def index
+    articles = Article.recent
+    paginated = paginator.call(
+      articles,
+      params: pagination_params,
+      base_url: request.url.to_s
+    )
+    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
+    render json: serializer.new(paginated.items, options), status: :ok
+  end 
+
+  def serializer
+    ArticleSerializer
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
+  end
+
+end
